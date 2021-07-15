7dee7312c831f0e52106803bb295e5e8569e6259
Hyoukjoo <jo920208@gmail.com>
2021-07-16 08:32:30 +0900

    💄 Molecule > Normal Button 추가

---
+++ client/src/components/molecules/Button/normal/normal-button.js
@@ -0,0 +1,31 @@
+import "./style.scss";
+import { createButtonAtom } from "@atoms/Button/button.js";
+import { createLinkAtom } from "../../../atoms/Text/link.js";
+
+const BUTTON_SIZE = ["medium", "large"];
+
+export const createNormalButton = ({
+  onClick,
+  label,
+  size = "medium",
+  disabled,
+}) => {
+  const isNotSupported = !BUTTON_SIZE.includes(size);
+
+  if (isNotSupported) {
+    throw Error(`This ${size} size is not supported`);
+  }
+
+  const buttonSize = `button-${size}`;
+  const fontSize = size === "medium" ? "small" : "medium";
+
+  const $button = createButtonAtom({ onClick });
+  const $label = createLinkAtom({ type: "label", size: fontSize, text: label });
+
+  $button.disabled = disabled;
+  $button.classList.add("normal-button", buttonSize);
+
+  $button.append($label);
+
+  return $button;
+};
