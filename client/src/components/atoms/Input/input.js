04be6e94867e78af37ef3069be2ac722eed7e1de
Hyoukjoo <jo920208@gmail.com>
2021-07-16 08:54:46 +0900

    💄 Atom > TextInput 추가

---
+++ client/src/components/atoms/Input/input.js
@@ -0,0 +1,22 @@
+import "./style.scss";
+import { INPUT_SIZE_TYPE } from "./constant.js";
+
+export const createInputAtom = ({
+  onChange = () => {},
+  placeholder = "",
+  size = "large",
+}) => {
+  const isNotSupportedSize = !INPUT_SIZE_TYPE.includes(size);
+
+  if (isNotSupportedSize) {
+    throw Error(`This ${size} size is not supported`);
+  }
+
+  const $input = document.createElement("input");
+
+  $input.classList.add("atom", `atom-input-size-${size}`);
+  $input.placeholder = placeholder;
+  $input.addEventListener("input", onChange);
+
+  return $input;
+};
