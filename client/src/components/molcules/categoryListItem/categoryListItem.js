2974d3bf8cdb70044fb7008a1fba879b15a79d36
dongqui <dongqui9@gmail.com>
2021-07-16 00:55:14 +0900

    💄 Molcule > CategoryListItem

---
+++ client/src/components/molcules/categoryListItem/categoryListItem.js
@@ -0,0 +1,14 @@
+import "./style.scss";
+import { createTextAtom } from "@atoms/text/text.js";
+import { createImgBoxAtom } from "@atoms/imgBox/imgBox.js";
+
+export const createCategoryListItemMolcule = ({ text }) => {
+  const $categoryListItem = document.createElement("div");
+  const $imgBox = createImgBoxAtom({ type: "small" });
+  const $text = createTextAtom({ type: "span", size: "x-small", text });
+
+  $categoryListItem.append($imgBox, $text);
+  $categoryListItem.classList.add("category-list-item");
+
+  return $categoryListItem;
+};
