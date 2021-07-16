35726bf31fd269fa125a336397f392ae5a940ff4
dongqui <dongqui9@gmail.com>
2021-07-17 03:39:01 +0900

    💄 Moelcule > dropdown

---
+++ client/src/components/molecules/dropdown/index.js
@@ -0,0 +1,35 @@
+import "./style.scss";
+import { createButtonAtom } from "@atoms/button/button.js";
+
+const createDropdownMolecule = ({ dropper, items, top, right, left, down }) => {
+  const $dropdown = document.createElement("div");
+  const $dropdownList = document.createElement("ul");
+  const $dropbutton = createButtonAtom({
+    onClick: () => {
+      $dropdownList.classList.toggle("dropdown-list-active");
+    },
+  });
+
+  $dropdown.append($dropbutton, $dropdownList);
+  $dropbutton.append(dropper);
+  $dropdownList.append(...items);
+  $dropdown.classList.add("dropdown");
+  $dropdownList.classList.add("dropdown-list");
+
+  if (top !== undefined) {
+    $dropdownList.style.top = top;
+  }
+  if (right !== undefined) {
+    $dropdownList.style.right = right;
+  }
+  if (left !== undefined) {
+    $dropdownList.style.left = left;
+  }
+  if (down !== undefined) {
+    $dropdownList.style.down = down;
+  }
+
+  return $dropdown;
+};
+
+export default createDropdownMolecule;
