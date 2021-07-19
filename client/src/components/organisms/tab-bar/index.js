aa7761ae5d82f9461aa8adefc56a41a8defe2e98
dongqui <dongqui9@gmail.com>
2021-07-20 01:30:34 +0900

    ✨ tab active

--- client/src/components/organisms/tab-bar/index.js
+++ client/src/components/organisms/tab-bar/index.js
@@ -1,14 +1,23 @@
 import "./style.scss";
 import { createTabButtonMolecule } from "@molecules";
 
+function tabButtonHandler(callback, idx) {
+  return function (e) {
+    const $activeButton = document.querySelector(".tab-button.active");
+    $activeButton && $activeButton.classList.remove("active");
+    e.target.closest("button").classList.add("active");
+    callback();
+  };
+}
+
 const createTabBarOrganism = () => {
   const $tabBar = document.createElement("div");
   $tabBar.classList.add("tab-bar");
 
-  ["판매목록", "채팅", "관심목록"].forEach((tab) => {
+  ["판매목록", "채팅", "관심목록"].forEach((tab, idx) => {
     const $tabWrapper = document.createElement("div");
     const $tabButton = createTabButtonMolecule({
-      onClick: () => {},
+      onClick: tabButtonHandler(() => {}, idx),
       label: tab,
     });
     $tabBar.append($tabWrapper);
