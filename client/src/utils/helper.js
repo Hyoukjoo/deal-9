bc439c7d08d36f05c811a768abd54ea21dfa6aec
dongqui <dongqui9@gmail.com>
2021-07-19 15:04:04 +0900

    ✨  price format helper

---
+++ client/src/utils/helper.js
@@ -0,0 +1,10 @@
+export const priceFormat = (price, unit) => {
+  const _price = (+price).toLocaleString();
+  if (unit === "원") {
+    return `${_price}unit`;
+  } else if (unit === "₩") {
+    return `${unit}${_price}`;
+  } else {
+    return _price;
+  }
+};
