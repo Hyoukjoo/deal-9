31a0626a4e7099b72ef937324bc9fba5332386a1
Hyoukjoo <jo920208@gmail.com>
2021-07-19 16:52:37 +0900

    ⚡️ 이미지 src가 없으면 빈 공간 노출하도록 수정

--- client/src/components/molecules/product-img/index.js
+++ client/src/components/molecules/product-img/index.js
@@ -3,11 +3,14 @@ import { createImgBoxAtom } from "@atoms";
 
 const createProductImgMoleclue = ({ type, src }) => {
   const $imgBox = createImgBoxAtom({ type });
-  const $img = document.createElement("img");
-  $img.src = src;
 
-  $imgBox.append($img);
   $imgBox.classList.add("product-img");
+
+  if (src !== undefined) {
+    const $img = document.createElement("img");
+    $img.src = src;
+    $imgBox.append($img);
+  }
 
   return $imgBox;
 };
