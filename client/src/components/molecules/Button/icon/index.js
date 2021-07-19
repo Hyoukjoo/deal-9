bfe0c785de5c1793298ad061a3eded032dc18e39
dongqui <dongqui9@gmail.com>
2021-07-19 21:15:07 +0900

    💄 Main header, button 스타일 수정
    아이콘에 색 적용, default button 스타일 버그 수정

--- client/src/components/molecules/Button/icon/index.js
+++ client/src/components/molecules/Button/icon/index.js
@@ -1,9 +1,9 @@
 import "./style.scss";
 import { createIconAtom } from "@atoms";
 
-const createIconButtonMolecule = ({ type }) => {
+const createIconButtonMolecule = ({ type, color }) => {
   const $iconButton = document.createElement("button");
-  const $icon = createIconAtom({ type });
+  const $icon = createIconAtom({ type, color });
 
   $iconButton.appendChild($icon);
   $iconButton.classList.add("icon-button");
--- client/src/components/organisms/main-header/index.js
+++ client/src/components/organisms/main-header/index.js
@@ -1,25 +1,31 @@
 import "./style.scss";
 import { createIconAtom, createLinkAtom, createButtonAtom } from "@atoms";
 import { createIconButtonMolecule } from "@molecules";
+import { WHITE } from "@common/styles/color.js";
 
 const createMainHeaderOrganism = ({ location }) => {
   const $header = document.createElement("header");
 
-  const $left = createIconButtonMolecule({ type: "category" });
+  const $left = document.createElement("div");
+  const $categoryButton = createIconButtonMolecule({
+    type: "category",
+    color: WHITE,
+  });
+  $left.append($categoryButton);
   $left.classList.add("main-header-left");
 
-  // TODO: dropbox 추가
+  // TODO: dropdown 추가
   const $middle = document.createElement("div");
   $middle.append(
-    createIconAtom({ type: "map-pin" }),
+    createIconAtom({ type: "map-pin", color: WHITE }),
     createLinkAtom({ type: "span", size: "medium", text: location || "장소" })
   );
   $middle.classList.add("main-header-middle");
 
   const $right = document.createElement("div");
   $right.append(
-    createIconButtonMolecule({ type: "user" }),
-    createIconButtonMolecule({ type: "menu" })
+    createIconButtonMolecule({ type: "user", color: WHITE }),
+    createIconButtonMolecule({ type: "menu", color: WHITE })
   );
   $right.classList.add("main-header-right");
 
