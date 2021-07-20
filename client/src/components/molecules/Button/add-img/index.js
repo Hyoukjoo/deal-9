61078394cf07e1a73df93acd484f046673e95134
dongqui <dongqui9@gmail.com>
2021-07-21 02:24:19 +0900

    ⚡️포스트 인풋 스타일 추가, add-img-buttom 스타일 변경

--- client/src/components/molecules/Button/add-img/index.js
+++ client/src/components/molecules/Button/add-img/index.js
@@ -1,11 +1,11 @@
 import "./style.scss";
 import { createImgBoxAtom, createIconAtom, createTextAtom } from "@atoms";
+import { GRAY1 } from "@common/styles/color.js";
 
 const createAddImgButtonMolecule = ({ imgCount }) => {
   const $addImgButton = document.createElement("button");
   const $imgBox = createImgBoxAtom({ type: "medium" });
-  // TODO: icon 색상 변경
-  const $imgIcon = createIconAtom({ type: "image" });
+  const $imgIcon = createIconAtom({ type: "image", color: GRAY1 });
   const $imgCountText = createTextAtom({
     type: "span",
     size: "x-small",
