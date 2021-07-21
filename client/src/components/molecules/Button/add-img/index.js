8554dab703c2b76a6774e2cdb3e97043c46b0bf9
dongqui <dongqui9@gmail.com>
2021-07-21 17:41:10 +0900

    ✨ POST 페이지 이벤트 핸들링 추가

--- client/src/components/molecules/Button/add-img/index.js
+++ client/src/components/molecules/Button/add-img/index.js
@@ -1,23 +1,31 @@
 import "./style.scss";
-import { createImgBoxAtom, createIconAtom, createTextAtom } from "@atoms";
+import {
+  createImgBoxAtom,
+  createIconAtom,
+  createTextAtom,
+  createInputAtom,
+} from "@atoms";
 import { GRAY1 } from "@common/styles/color.js";
 
-const createAddImgButtonMolecule = ({ imgCount }) => {
-  const $addImgButton = document.createElement("button");
+const createAddImgButtonMolecule = ({ onChange }) => {
+  const $label = document.createElement("label");
   const $imgBox = createImgBoxAtom({ type: "medium" });
   const $imgIcon = createIconAtom({ type: "image", color: GRAY1 });
   const $imgCountText = createTextAtom({
     type: "span",
     size: "x-small",
-    text: `${imgCount} / 10`,
+    text: "0 / 10",
   });
+  const $fileInput = createInputAtom({ type: "file" });
+  $fileInput.multiple = true;
+  $fileInput.addEventListener("change", onChange);
 
-  $addImgButton.appendChild($imgBox);
-  $addImgButton.classList.add("add-img-button");
+  $label.append($imgBox, $fileInput);
+  $imgBox.classList.add("add-img-button");
   $imgBox.appendChild($imgIcon);
   $imgBox.appendChild($imgCountText);
 
-  return $addImgButton;
+  return $label;
 };
 
 export default createAddImgButtonMolecule;
