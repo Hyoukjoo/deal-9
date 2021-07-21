52fc2ae27252f30533d12d5f4021c6433a13d063
dongqui <dongqui9@gmail.com>
2021-07-22 03:21:11 +0900

    ⚡️테이블 수정, db option - multipleStatements: true

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -3,11 +3,12 @@ import env from "../../config/env.js";
 
 const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = env;
 
-const pool = mysql.createPool({
-  host: MYSQL_HOST,
-  user: MYSQL_USER,
-  password: MYSQL_PASSWORD,
-  database: MYSQL_DATABASE,
+const connection = await mysql.createConnection({
+  host: MYSQL_HOST || "mysql_server",
+  user: MYSQL_USER || "deal-9",
+  password: MYSQL_PASSWORD || "secret",
+  database: MYSQL_DATABASE || "test_db",
+  multipleStatements: true,
 });
 
-export default pool;
+export default connection;
