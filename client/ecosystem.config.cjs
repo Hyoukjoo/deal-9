5077525bc9806fd65d2b3166099d16eaadc7e66e
Hyoukjoo <jo920208@gmail.com>
2021-07-22 12:52:17 +0900

    🚀 배포 업데이트

--- client/ecosystem.config.cjs
+++ client/ecosystem.config.cjs
@@ -3,9 +3,8 @@ module.exports = {
     {
       name: "deal-9",
       script: "./src/server.js",
-      instances: 1,
+      instances: 0,
       exec_mode: "cluster",
-      watch: true,
     },
   ],
 };
