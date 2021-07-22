9d57562422eca6cd6f8c37e3e2c3a9edab7fb71e
dongqui <dongqui9@gmail.com>
2021-07-23 05:03:21 +0900

    💄 스타일 변경

--- client/src/components/molecules/category-list-Item/index.js
+++ client/src/components/molecules/category-list-Item/index.js
@@ -1,14 +1,17 @@
 import "./style.scss";
-import { createTextAtom } from "@atoms";
+import { createTextAtom, createButtonAtom } from "@atoms";
 import { createProductImgMolecule } from "@molecules";
 
-const createCategoryListItemMolecule = ({ text, src }) => {
-  const $categoryListItem = document.createElement("div");
+const createCategoryListItemMolecule = ({ text, src, isSelected, onClick }) => {
+  const $categoryListItem = createButtonAtom({ onClick });
   const $productImg = createProductImgMolecule({ type: "small", src });
   const $text = createTextAtom({ type: "span", size: "x-small", text });
 
   $categoryListItem.append($productImg, $text);
   $categoryListItem.classList.add("category-list-item");
+  if (isSelected) {
+    $categoryListItem.classList.add("selected");
+  }
 
   return $categoryListItem;
 };
