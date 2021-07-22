5f35b480c05a0e306364c2a39d2a5129090dac8c
dongqui <dongqui9@gmail.com>
2021-07-22 14:13:41 +0900

    🎨 Products 컨트롤러 사용, 에러 핸들링

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -1,32 +1,12 @@
 import { Router } from "express";
-import ProductRepository from "../repositories/products.js";
+import ProductController from "../controllers/ProductController.js";
+import AuthMiddleware from "../middlewares/AuthMiddleware.js";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  const { locationId, categoryId } = req.query;
-  const proudcts = await ProductRepository.getProducts({
-    locationId,
-    categoryId,
-  });
-  res.status(200).send(proudcts);
-});
+router.get("/", ProductController.getProducts);
 
-router.post("/", async (req, res) => {
-  const { userId, locationId, title, content, status, images, categories } =
-    req.body;
-  await ProductRepository.createProduct({
-    userId,
-    locationId,
-    title,
-    content,
-    status,
-    images,
-    categories,
-  });
-
-  res.status(204).send();
-});
+router.post("/", ProductController.createProduct);
 
 router.put("/:id", (req, res) => {});
 router.delete("/:id", (req, res) => {});
