bc52fa80e488a7b4f54d28fb0795b944c42d0811
Hyoukjoo <jo920208@gmail.com>
2021-07-22 16:53:19 +0900

    🚑 ecosystem 변경

--- client/ecosystem.config.cjs
+++ client/ecosystem.config.cjs
@@ -2,9 +2,10 @@ module.exports = {
   apps: [
     {
       name: "deal-9",
-      script: "./src/server.js",
+      script: "./dist/server.js",
       instances: 0,
       exec_mode: "cluster",
+      watch: true,
     },
   ],
 };
